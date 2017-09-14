ba37897f0f27efc7e028626cfa6b0b7cd253e7d0
flan the dev <flanthedev@gmail.com>
2017-09-14 22:46:19 +0800

    complete navigate between pages

--- .next/dist/pages/index.js
+++ .next/dist/pages/index.js
@@ -8,23 +8,37 @@ var _react = require("react");
 
 var _react2 = _interopRequireDefault(_react);
 
+var _link = require("next/dist/lib/link.js");
+
+var _link2 = _interopRequireDefault(_link);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var _jsxFileName = "/home/chris/Documents/hello-next/pages/index.js?entry";
+
 
 var Index = function Index() {
   return _react2.default.createElement("div", {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 2
+      lineNumber: 4
     }
-  }, _react2.default.createElement("p", {
+  }, _react2.default.createElement(_link2.default, { href: "/about", __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    }
+  }, _react2.default.createElement("button", {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 3
+      lineNumber: 6
+    }
+  }, "Go to about page")), _react2.default.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
     }
   }, "Hello Next.js"));
 };
 
 exports.default = Index;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIkluZGV4Il0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7OztBQUFBLElBQU0sUUFBUSxTQUFSLEFBQVEsUUFBQTt5QkFDWixjQUFBOztnQkFBQTtrQkFBQSxBQUNFO0FBREY7QUFBQSxHQUFBLGtCQUNFLGNBQUE7O2dCQUFBO2tCQUFBO0FBQUE7QUFBQSxLQUZVLEFBQ1osQUFDRTtBQUZKLEFBTUE7O2tCQUFBLEFBQWUiLCJmaWxlIjoiaW5kZXguanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL2hvbWUvY2hyaXMvRG9jdW1lbnRzL2hlbGxvLW5leHQifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIkxpbmsiLCJJbmRleCJdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7OztBQUFBLEFBQU87Ozs7Ozs7OztBQUVQLElBQU0sUUFBUSxTQUFSLEFBQVEsUUFBQTt5QkFDWixjQUFBOztnQkFBQTtrQkFBQSxBQUNFO0FBREY7QUFBQSxHQUFBLGtCQUNFLEFBQUMsZ0NBQUssTUFBTixBQUFXO2dCQUFYO2tCQUFBLEFBQ0U7QUFERjtxQkFDRSxjQUFBOztnQkFBQTtrQkFBQTtBQUFBO0FBQUEsS0FGSixBQUNFLEFBQ0UsQUFFRixzQ0FBQSxjQUFBOztnQkFBQTtrQkFBQTtBQUFBO0FBQUEsS0FMVSxBQUNaLEFBSUU7QUFMSixBQVNBOztrQkFBQSxBQUFlIiwiZmlsZSI6ImluZGV4LmpzP2VudHJ5Iiwic291cmNlUm9vdCI6Ii9ob21lL2NocmlzL0RvY3VtZW50cy9oZWxsby1uZXh0In0=
